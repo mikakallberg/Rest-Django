de3d4a8771fc3c1e718ea48bd10c74ebae0359c3
mikakallberg <mikaela.kallberg@hotmail.com>
2022-08-02 17:53:38 +0000

    Add queryset

--- profiles/views.py
+++ profiles/views.py
@@ -2,19 +2,36 @@
 # from rest_framework import status
 # from rest_framework.views import APIView
 # from rest_framework.response import Response
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
+from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
-from drf_api.permissions import IsOwnerOrReadOnly
 
 
 class ProfileList(generics.ListAPIView):
     """
     List all profiles.
-    No create view as profile creation is handled by django signals.
+    No create view as profile creation is 
+    handled by django signals.
     """
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count(
+            'owner__following', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'posts_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at',
+        'owner__followed__created_at',
+    ]
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
@@ -22,14 +39,19 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     Retrieve or update a profile if you're the owner.
     """
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
 
 
 # class ProfileList(APIView):
 #     """
 #     List all profiles
-#     No Create view (post method), as profile creation handled by django signals
+#     No Create view (post method), as profile creation 
+# handled by django signals
 #     """
 #     def get(self, request):
 #         profiles = Profile.objects.all()
