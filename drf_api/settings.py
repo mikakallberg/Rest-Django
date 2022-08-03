0d4707396b9553508ec36ad08652bce1b00be3e1
mikakallberg <mikaela.kallberg@hotmail.com>
2022-08-03 15:33:58 +0000

    deployment commit

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -66,7 +66,7 @@ SECRET_KEY = {
 }
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = ['moments-drf-api-app.herokuapp.com', 'localhost']
 
